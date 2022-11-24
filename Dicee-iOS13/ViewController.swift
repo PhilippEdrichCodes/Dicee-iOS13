602fd04de38e3970f53af6068fa4d028882e8365
PhilippEdrichCodes <philipp@edrich.codes>
2022-11-24 17:34:05 +0100

    went a little faster in the tut

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // These two IBOutlets allows to reference the UI elements of the dice
+    @IBOutlet weak var diceImageViewLeft: UIImageView!
+    @IBOutlet weak var diceImageViewRight: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    
+    // Button Pressed
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        // create a die
+        let diceFaces = [
+            UIImage(named: "DiceOne"),
+            UIImage(named: "DiceTwo"),
+            UIImage(named: "DiceThree"),
+            UIImage(named: "DiceFour"),
+            UIImage(named: "DiceFive"),
+            UIImage(named: "DiceSix")
+        ]
+        
+        // get the random images
+        diceImageViewLeft.image = diceFaces.randomElement() as? UIImage
+        diceImageViewRight.image = diceFaces.randomElement() as? UIImage
+    }
 
 }
 
